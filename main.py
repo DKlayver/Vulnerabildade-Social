d52fc3df8493e76c84a8b706b30dd86735368ff2
DKlayver <92654447+DKlayver@users.noreply.github.com>
2024-05-25 19:13:05 -0300

    adicionando Main

---
+++ main.py
@@ -0,0 +1,30 @@
+'''
+                        ---Grupo 01---
+    Projeto Acadêmico da matério de Big Data com Professor Max Castro
+
+    Integrantes do grupo:
+
+        Davi Klayver Dantas Lima,
+        Laion de Araújo Hora,
+        Juliana Rezende dos Santos,
+        Társis de Nascimenteo Santos.
+
+    Objetivo:
+
+        A partir do tema Vulnerabilidade Social, iremos explorar um dataBase público disponível
+        no site: http://ivs.ipea.gov.br/index.php/pt/biblioteca  e extrair as seguintes informações:
+
+            1. Relação entre o emprego informal e a expectativa de vida;
+            2. Relação entre o desemprego e a escolaridade;
+            3. Relação entre o índice de gini (medida para a desigualdade) e a escolaridade.
+
+        Após isso, será exibidaas informações obtidas em um PowerBi para a melhor apresentação.
+
+
+'''
+#pip install matplotlib
+#pip install pandas
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
